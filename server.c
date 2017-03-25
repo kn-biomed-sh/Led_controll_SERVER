56c230c89655e204fc2a018482a7f5453180a8e4
kn-biomed-sh <kn.biomed.sh@gmail.com>
2017-03-25 21:12:39 +0100

    Working server

--- server.c
+++ server.c
@@ -5,71 +5,50 @@
 #include <netinet/in.h>
 #include <string.h>
 #include <stdlib.h>
+#include "LedON.h"
 
-int main(){
-  init();
-  Led(100);
+int main()
+{
+	init(); 
+	int welcomeSocket, newSocket, portNum, clientLen, nBytes;
+	unsigned char buffer[1024];
+	struct sockaddr_in serverAddr;
+	struct sockaddr_storage serverStorage;
+	socklen_t addr_size;
+	int i = 0;
+	int PWM_value;
+	int PWM_value2;
   
-  int welcomeSocket, newSocket, portNum, clientLen, nBytes;
-  unsigned char buffer[1024];
-  //int buffer[1024];
-  struct sockaddr_in serverAddr;
-  struct sockaddr_storage serverStorage;
-  socklen_t addr_size;
-  int i;
-  int PWM_value;
-
-  welcomeSocket = socket(PF_INET, SOCK_STREAM, 0);
-
-  portNum = 7891;
+	welcomeSocket = socket(PF_INET, SOCK_STREAM, 0);
+	portNum = 8080;
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(portNum);
+	serverAddr.sin_addr.s_addr = inet_addr("172.24.1.1");
+	memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);  
+	bind(welcomeSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
+	addr_size = sizeof serverStorage;
   
-  serverAddr.sin_family = AF_INET;
-  serverAddr.sin_port = htons(portNum);
-  serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-  memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);  
-
-bind(welcomeSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
-
-  if(listen(welcomeSocket,5)==0)
-    printf("Listening\n");
-  else
-    printf("Error\n");
-
-  addr_size = sizeof serverStorage;
-
-  /*loop to keep accepting new connections*/
-  while(1){
-    newSocket = accept(welcomeSocket, (struct sockaddr *) &serverStorage, &addr_size);
-    /*fork a child process to handle the new connection*/
-    if(!fork())
-    {
-      nBytes = 1;
-      /*loop while connection is live*/
-      while(nBytes!=0){
-        nBytes = recv(newSocket,buffer,1024,0);
+	if(listen(welcomeSocket,5)==0)    printf("Listening\n");
+	else    printf("Error\n");
+	close(newSocket);
   
-        for (i=0;i<nBytes-1;i++)
-        {
-			PWM_value = buffer[i];
-			PWM_value -= 48;
-			printf("%i\n\n", PWM_value);
-			//printf("%i\n\n", buffer[i]);
-          buffer[i] = toupper(buffer[i]);
-        }
-
-        send(newSocket,buffer,nBytes,0);
-      }
-      close(newSocket);
-
-      exit(0);
-    }
-    
-    /*if parent, close the socket and go back to listening new requests*/
-    else
-    {
-      close(newSocket);
-    }
-  }
-
-  return 0;
+	/*loop to keep accepting new connections*/
+	while(1)
+	{
+		newSocket = accept(welcomeSocket, (struct sockaddr *) &serverStorage, &addr_size);
+		nBytes = 1;
+      
+		/*loop while connection is live*/
+		while(nBytes!=0)
+		{
+			i++;
+			nBytes = recv(newSocket,buffer,1024,0);  
+			PWM_value = (int)buffer[0];
+			printf("#%d		PWM: %d\n", i, PWM_value); 
+			Led_One(PWM_value);
+		}
+		//close(newSocket);
+		//exit(0);
+	}
+	return 0;
 }
